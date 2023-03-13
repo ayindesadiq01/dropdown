bb98c54b2fb993bd7a74b14b60d63a962e16956b
ayindesadiq01 <ayindesadiq01@gmail.com>
2023-03-13 19:29:06 +0100

    dropdown

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+const arrowDown = document.querySelector('.arrow-down');
+const arrowUp = document.querySelector('.arrow-up');
+const dropDown = document.querySelectorAll('.dropdown-features');
+const hidden = document.querySelector('.hidden');
+const dropContent = document.querySelectorAll('.dropdown-content');
+const container = document.querySelector('.container')
+const closeIcon = document.querySelector('.close-icon');
+const menuIcon = document.querySelector('.menu-icon');
+const bar = document.querySelector('.bar')
+const barRemove = document.querySelector('.bar-remove')
+const overlay =  document.querySelector('.overlay');
+
+for(let i=0; i < dropDown.length; i++ ) {
+dropDown[i].addEventListener('click', function(){
+ dropContent.classList.toggle('hidden');
+})
+}
+
+menuIcon.addEventListener('click', function() {
+ bar.classList.remove('bar-remove');
+ overlay.classList.remove('bar-remove');
+})
+closeIcon.addEventListener('click', function() {
+ bar.classList.add('bar-remove');
+ overlay.classList.add('bar-remove');
+})
+
